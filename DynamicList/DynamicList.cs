b41bf5e63b8db0cfcd41864bdd912398e8e60268
Антон Корытко < >
2017-09-07 22:48:36 +0300

    Added DynamicList class and its unit tests.

---
+++ DynamicList/DynamicList.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DynamicList
+{
+    public class DynamicList<T> : IEnumerable<T>
+    {
+        public T[] dynamicList;
+
+        public DynamicList()
+        {
+            dynamicList = new T[0];
+        }
+
+        public T[] Items
+        {
+            get { return dynamicList; }
+        }
+
+        public int Count
+        {
+            get { return dynamicList.Length; }
+        }
+
+        public void Add(T item)
+        {
+            Array.Resize(ref dynamicList, dynamicList.Length + 1);
+            dynamicList[dynamicList.Length - 1] = item;
+        }
+
+        public void Remove(T item)
+        {
+            dynamicList = dynamicList.Except(new T[] {item}).ToArray();
+        }
+
+        public void RemoveAt(int removalIndex)
+        {
+            if ((removalIndex < 0) || (removalIndex > dynamicList.Length - 1)) return;
+            for (int i = removalIndex; i < dynamicList.Length - 1; i++)
+                dynamicList[i] = dynamicList[i + 1];
+
+            Array.Resize(ref dynamicList, dynamicList.Length - 1);
+        }
+
+        public void Clear()
+        {
+            dynamicList = new T[0];
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (T item in dynamicList)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ UnitTest/UnitTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DynamicList;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestItems()
+        {
+            int[] desiredList = new int[] { 1, 2, 3 };
+            DynamicList<int> list = new DynamicList<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            for (int i = 0; i < list.Count; i++)
+                Assert.AreEqual(list.Items[i], desiredList[i]);
+        }
+
+        [TestMethod]
+        public void TestCount()
+        {
+            int[] desiredList = new int[] { 1, 2, 3, 4, 5 };
+            DynamicList<int> list = new DynamicList<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            Assert.AreEqual(list.Count, desiredList.Length);
+        }
+
+        [TestMethod]
+        public void TestAdd()
+        {
+            int[] desiredList = new int[] {123, 456, 789};
+            DynamicList<int> list = new DynamicList<int>();
+            list.Add(123);
+            list.Add(456);
+            list.Add(789);
+            Assert.AreEqual(list.Items[0], desiredList[0]);
+            Assert.AreEqual(list.Items[1], desiredList[1]);
+            Assert.AreEqual(list.Items[2], desiredList[2]);
+        }
+
+        [TestMethod]
+        public void TestRemove()
+        {
+            int[] desiredList = new int[] { 123, 789 };
+            DynamicList<int> list = new DynamicList<int>();
+            list.Add(123);
+            list.Add(456);
+            list.Add(456);
+            list.Add(456);
+            list.Add(789);
+            list.Remove(456);
+            int i = 0;
+            foreach (int item in list)
+            {
+                Assert.AreEqual(item, desiredList[i]);
+                i++;
+            }
+        }
+
+        [TestMethod]
+        public void TestRemoveAt()
+        {
+            int[] desiredList = new int[] { 34, 56 };
+            DynamicList<int> list = new DynamicList<int>();
+            list.Add(12);
+            list.Add(34);
+            list.Add(56);
+            list.RemoveAt(0);
+            int i = 0;
+            foreach (int item in list)
+            {
+                Assert.AreEqual(item, desiredList[i]);
+                i++;
+            }
+        }
+    }
+}
